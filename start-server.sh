194719ce34b75afe6b6dda68b049d37e4309e271
categoricalcat <catufuzgu@gmail.com>
2025-06-11 19:48:05 -0300

    Add startup messages to server script: display memory allocation and launcher jar

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+echo "Starting server..."
+
+echo "Memory allocation: ${MEMORY_ALLOCATION:-6G}"
+echo "Fabric launcher jar: ${FABRIC_LAUNCHER_JAR}"
+
 exec java \
   -Xmx${MEMORY_ALLOCATION:-6G} \
   -Xms2G \
