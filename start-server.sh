8191dbf5bc5ea137ddba591df88e9b9fd8f461c6
categoricalcat <catufuzgu@gmail.com>
2025-06-11 19:53:26 -0300

    Update server properties and enhance start-server script: add .DS_Store to .gitignore, modify memory allocation messages, and update jar path

--- start-server.sh
+++ start-server.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
 
-echo "Starting server..."
+set -e
 
+PATH_TO_JAR="fabric-server-mc.${FABRIC_VERSION:-1.21.5}-loader.${FABRIC_LOADER_VERSION:-0.16.14}-launcher.${FABRIC_LAUNCHER_VERSION:-1.0.3}.jar"
+
+echo "Starting server..."
 echo "Memory allocation: ${MEMORY_ALLOCATION:-6G}"
-echo "Fabric launcher jar: ${FABRIC_LAUNCHER_JAR}"
+echo "Fabric launcher jar: ${PATH_TO_JAR}"
 
 exec java \
   -Xmx${MEMORY_ALLOCATION:-6G} \
   -Xms2G \
-  -jar "${FABRIC_LAUNCHER_JAR}" \
+  -jar "mc/${PATH_TO_JAR}" \
   nogui
